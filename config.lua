a5f649cf901106e3ad9e8453739be55a62173531
Haze <85334552+Haze-ts@users.noreply.github.com>
2022-05-01 19:54:50 +0200

    Ricostruzione del codice

---
+++ config.lua
@@ -0,0 +1,91 @@
+-- ██   ██ ███████ ███████       ███    ███ ███████ ███    ██ ██    ██ 
+-- ██   ██    ███  ██            ████  ████ ██      ████   ██ ██    ██ 
+-- ███████   ███   █████   █████ ██ ████ ██ █████   ██ ██  ██ ██    ██ 
+-- ██   ██  ███    ██            ██  ██  ██ ██      ██  ██ ██ ██    ██ 
+-- ██   ██ ███████ ███████       ██      ██ ███████ ██   ████  ██████                                                         
+
+Config = {}
+
+Config.TriggerMenuSim = "esx_cartesim:OpenSim"
+Config.TriggerMenuBilling = "METTI IL TUO TRIGGER"
+Config.TriggerTPM = "esx:tpm"
+Config.TriggerSmontaArmi = "METTI IL TUO TRIGGER"
+
+Config.ZgNotify = false
+
+
+Lang = {
+
+    --Main Menu
+        ["information"]             = "Informazioni Personali",
+        ['wallet']                  = "Documenti",
+        ["clothes"]                 = "Gestione Vestiti", 
+        ["accessory"]               = "Gestione Accessori", 
+        ["sim"]                     = "Gestione Sim",
+        ["smontaarmi"]              = "Gestione Armi",
+        ["thieft_menu"]             = "Menu Illegale", 
+        ["billing"]                 = "Gestione Fatture",
+        ['rockstar']                = 'Rockstar Editor',
+        ["administration"]          = "Amministrazione",
+    
+    --Player Info Menu
+        ["id"]                      = "ID: ",
+        ["name"]                    = "Steam: ",
+        ["job"]                     = "Lavoro: ",
+        ["job_grade"]               = "Grado: ",
+
+    --Wallet
+        ['show_document']           = 'Mostra Documento',
+        ['view_document']           = 'Guarda Documento',
+        ['show_driving_license']    = 'Mostra Patente',
+        ['view_driving_license']    = 'Guarda Patente',
+        ['show_weapon_license']     = 'Mostra Porto D\'armi',
+        ['view_weapon_license']     = 'Guarda Porto D\'armi',
+
+    --Clothes Menu
+        ["shirt"]                   = "Giacca",
+        ["pants"]                   = "Pantaloni",
+        ["shoes"]                   = "Scarpe",
+        ["bullet"]                  = "Giubbotto Antiproiettile",
+
+    --Accessory Menu
+        ["mask"]                    = "Maschera",
+        ["glass"]                   = "Occhiali",
+        ["hat"]                     = "Casco",
+        ["bag"]                     = "Borsa",
+
+    --Thief Menu
+        ["body_search"]             = "Perquisici",
+
+    --Rockstar Editor
+        ['openeditor']              = 'Apri Rockstar Editor',
+        ['startrec']                = 'Inizia Registrazione',
+        ['stoprec']                 = 'Interrompi Registrazione',
+
+    --Admin Menu
+        ["noclip"]                  = "NoClip",
+        ["repair_vehicle"]          = "Ripara Veicolo",
+        ["flip_vehicle"]            = "Ribalta Veicolo",
+        ["car"]                     = "Spawn Veicolo",
+        ["car_player"]              = "Dai Veicolo Player",
+        ["tpm"]                     = "Tpm",
+        ["wipe"]                    = "Wipe Giocatore",
+        ["giub"]                    = "Dai Giubbotto",
+        ["godmode"]                 = "GodMode",
+
+    --Notify
+        ['notify_administration']   = 'Non hai i permessi',
+        ['notify_player_not_found'] = 'Il giocatore non è online',
+        ['notify_search']           = 'Nessun player nelle vicinanze',
+        ['notify_repair_vehicle']   = 'Veicolo riparato con correttamente',
+        ['notify_no_vehicle']       = 'Non sei in un veciolo',
+        ['notify_flip_vehicle']     = 'Veicolo ribaltato',
+        ['notify_spawn_vehicle']    = 'Veicolo Spawnato',
+        ['notify_godmode_on']       = 'GodMode Attivata',
+        ['notify_godmode_off']      = 'GodMode Disattivata',
+        ['notify_coords_on']        = 'Coordinate Attivate',
+        ['notify_coords_off']       = 'Coordinate Disattivate',
+        ['notify_name_on']          = 'Nomi Attivati',
+        ['notify_name_off']         = 'Nomi Disattivati',
+
+}
