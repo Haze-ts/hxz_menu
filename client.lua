3d87a858631c0524609897b83651df39220610f0
Haze <85334552+Haze-ts@users.noreply.github.com>
2022-06-29 13:08:17 +0200

    Corretta la funzione di ricerca

--- client.lua
+++ client.lua
@@ -282,37 +282,43 @@ end
 
 --Thieft Menu
 function ThiefMenu()
-	ESX.UI.Menu.CloseAll()
+    ESX.UI.Menu.CloseAll()
 
-	ESX.UI.Menu.Open(
-	  'default', GetCurrentResourceName(), 'thief',
-	  {
-		  title    = "HXZ - Menu Illegale",
-		  align = 'top-left',
-		  elements = {	  			  
-			{label = Lang["body_search"],      value = 'perquisizione'},
-		  	}
-	  },function(data, menu)
+    ESX.UI.Menu.Open(
+      'default', GetCurrentResourceName(), 'thief',
+      {
+          title    = "HXZ - Menu Illegale",
+          align = 'top-left',
+          elements = {
+            {label = Lang["body_search"],      value = 'perquisizione'},
+              }
+      },function(data, menu)
 
-		local playerVicino, playerDistanza = ESX.Game.GetClosestPlayer()
-			if playerVicino ~= -1 and playerDistanza <= 3.0 then
-				if data.current.value == "perquisizione" then
-					menu.close()
-					OpenSearchMenu(playerVicino)
-				end
-			else
-				ESX.ShowNotification("Nessun player vicino")
-			end
-		end, function(data, menu)
-		menu.close()
-		MainMenu()
-	end)
-end
-
-
-function OpenSearchMenu(Player)
-	exports.ox_inventory:openInventory('player', GetPlayerServerId(Player))
-	return ESX.UI.Menu.CloseAll()
+        local playerVicino, playerDistanza = ESX.Game.GetClosestPlayer()
+            if playerVicino ~= -1 and playerDistanza <= 3.0 then
+                if data.current.value == "perquisizione" then
+                    if IsPedArmed(PlayerPedId(), 4) then
+                        if IsEntityPlayAnim(GetPlayerPed(playerVicino), 'anim@move_m@prisoner_cuffed_rc', 'aim_low_loop', 1) or IsEntityPlayAnim(GetPlayerPed(playerVicino), 'mp_arresting', 'idle', 1) then
+                            ESX.TriggerServerEvent('hxz:checkTarget', function(Target)
+                                if Target then
+                                    exports.ox_inventory:openNearbyInventory()
+                                    ExecuteCommand('me Sta Perquisendo')
+                                end
+                            end)
+                        else
+                            ESX.ShowNotification('Il giocatore deve avere le mani alzate')
+                        end
+                    else
+                        ESX.ShowNotification('Non sei armato')
+                    end
+                end
+            else
+                ESX.ShowNotification("Nessun player vicino")
+            end
+        end, function(data, menu)
+        menu.close()
+        MainMenu()
+    end)
 end
 
 --Rockstar Editor
